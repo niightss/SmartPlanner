975f21f949727defae41a91a08b6c9bc0b004e5b
noahm <noahmachenziemoran05@gmail.com>
2025-12-09 22:33:30 -0500

    Initial Commit

---
+++ SmartPlanner_v2Tests/SmartPlanner_v2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartPlanner_v2Tests.swift
+//  SmartPlanner_v2Tests
+//
+//  Created by Noah Moran on 12/9/25.
+//
+
+import Testing
+@testable import SmartPlanner_v2
+
+struct SmartPlanner_v2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
